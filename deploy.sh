d2bc569baa63abec80b6b22da8c6db6f194480df
RyeinKim <ryein6896@gmail.com>
2024-08-01 17:40:28 +0900

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc
+
+nvm install 20
+
+npm install -g pm2
+
 export NVM_DIR="/var/lib/jenkins/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
